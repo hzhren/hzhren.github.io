e7c9fda0dc426901e91d652428e703ba97c25b7f
hzhren <es_rainbow@foxmail.com>
2016-08-16 17:42:21 +0800

    主页修改-1

---
+++ javascript/auto-rem.js
@@ -0,0 +1,16 @@
+/**
+ * Created by hzhren on 2016/5/5.
+ */
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
